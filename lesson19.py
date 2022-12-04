71c96bb0a100224e377fa936b302dca077127e08
avivhefetz <109032474+avivhefetz@users.noreply.github.com>
2022-12-04 17:19:26 +0200

    Update lesson19.py

--- lesson19.py
+++ lesson19.py
@@ -1,3 +1,11 @@
-list10 = ["pokimon", "Son Goko", "eleven", 11, 1000, 152]
+list10 = ["pokemon", "Son Goko", "eleven", 11, 1000, 152]
 list10.reverse()
 print(list10)
+
+
+
+number = ''
+list1 =[1,2,3,4]
+for i in list1:
+    number = number + str(i)
+print(int(number))
