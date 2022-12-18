37731aeb32027823fd33d21cf386952f12bd5d3e
avivhefetz <109032474+avivhefetz@users.noreply.github.com>
2022-12-18 18:45:15 +0200

    Create lesson20.py

---
+++ lesson20.py
@@ -0,0 +1,21 @@
+set1 = {0,}
+def function(set1):
+    a = int(input("print number: "))
+    b = int(input("again: "))
+    set1.add(a)
+    set1.add(b)
+    sum(set1)
+print(function(set1))
+
+
+
+
+set1 = {"",}
+def function(set1):
+    a = str(input("print string: "))
+    b = str(input("again: "))
+    set1.add(a)
+    set1.add(b)
+    sum(set1)
+print(function(set1))
+
